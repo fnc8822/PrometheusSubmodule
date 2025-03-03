1e3a32c7e79305bf62046dff307a4cbb93366a8b
fnc8822 <francocirrincione@mi.unc.edu.ar>
2025-03-03 14:13:39 -0300

    werror fixes

--- include/main.h
+++ include/main.h
@@ -4,16 +4,16 @@
  * includes global variables and function prototypes
  */
 
-extern bool cpu_enabled = false;
-extern bool memory_enabled = true;
-extern bool battery_enabled = false;
-extern bool avg_load_enabled = false;
-extern bool cpu_temp_enabled = false;
-extern bool cpu_speed_enabled = false;
-extern bool processes_enabled = false;
-extern bool sys_calls_enabled = false;
-extern bool disk_io_enabled = false;
-extern bool network_enabled = false;
+extern bool cpu_enabled;
+extern bool memory_enabled;
+extern bool battery_enabled;
+extern bool avg_load_enabled;
+extern bool cpu_temp_enabled;
+extern bool cpu_speed_enabled;
+extern bool processes_enabled;
+extern bool sys_calls_enabled;
+extern bool disk_io_enabled;
+extern bool network_enabled;
 
 
 /**
